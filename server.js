8d68893998bc53281b1e633184ebd0b0ea349cbe
Max Ainatchi <maxwell.ainatchi@gmail.com>
2017-11-29 23:38:48 -0500

    Did some stuff

--- server.js
+++ server.js
@@ -14,6 +14,20 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/addFriend", async (req, res, next) => {
+	service.
+	res.render("addFriend")
+});
+
+app.post("/logout", async (req, res, next) => {
+	if (!service.user) {
+		res.status(400).send("not logged in")
+	} else {
+		service.logout()
+	}
+	res.redirect("/")
+});
+
 app.post("/login", async (req, res, next) => {
 	if (service.user) {
 		res.status(400).send("already logged in")
