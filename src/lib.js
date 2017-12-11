52e6582fb3824f5ee79a977d36001c138c5e3ab1
Jacob Abbott <jaa661@nyu.edu>
2017-12-10 23:13:10 -0500

    AddTag biz
    Descriptod

--- src/lib.js
+++ src/lib.js
@@ -39,13 +39,45 @@ let userGroups = function (username) {
     )).then(results => results.map(result => result.group_name))
 }
 
+let getVisibleContent = async function (username) {
+    let groups = await utils.validateUsername(username).then(() => db.query(
+        `SELECT FriendGroup.group_name
+		 FROM FriendGroup
+		 WHERE username='${username}';`
+    )).then(results => results.map(result => result.group_name))
+	let content = []
+	for (group in groups){
+       let contentx = await db.query(
+            `SELECT content.content_name,id
+		 FROM share
+		 WHERE group_name='${group}';`
+        )
+		for (i = 0; i < contentx.length; i++){
+			content.push(contentx[i])
+		}
+	}
+    let contenty = await db.query(
+        `SELECT content.content_name,id
+		 FROM content
+		 WHERE public='1';`
+    )
+    for (i = 0; i < contenty.length; i++){
+        content.push(contenty[i])
+    }
+	return content
+}
 
-let getUsernames = function (firstname, lastname) {
-    return db.query(
+
+let getUsernames = async function (firstname, lastname) {
+    var userlist = []
+    userlist = await db.query(
         `SELECT username
-		 FROM Person 
+		 FROM Person
 		 WHERE first_name='${firstname}' AND last_name='${lastname}';`
     ).then(results => results.map(result => result.username))
+    console.log("user list is:" + userlist.length)
+    console.log(userlist[0])
+    return userlist[0]
 }
 
 
@@ -70,8 +102,44 @@ let addContent = function(username, filePath, title, pub) {
 		`INSERT INTO Content (username, file_path, content_name, public)
 		VALUES ('${username}','${filePath}','${title}','${pub}');`
 	)).then(() => {
+        return true
+    })
+}
+
+let isVisible = async function(username, content) {
+    let groupsShared = await utils.validateUsername(username).then(() => db.query(
+        `SELECT group_name
+		 FROM Share
+		 WHERE id='${content.id}';`
+    )).then(results => results.map(result => result.group_name))
+    let friendGroups = await db.query(
+        `SELECT group_name
+		 FROM member
+		 WHERE username='${username}';`
+    ).then(results => results.map(result => result.group_name))
+    for (i =0;i<groupsShared.length;i++){
+        for (j =0;j<friendGroups.length;j++)
+        {
+            if (friendGroups[i] === groupsShared[j])
+                return true
+        }
+    }
+    return false
+}
+
+let addTag = async function(usernamex, content, usernamey) {
+    let approved = false;
+    if (usernamex === usernamey)
+    	approved = true;
+    if (isVisible(usernamey, content)) {
+		await utils.validateUsername(usernamex).then(() => db.query(
+			`INSERT INTO Tag (id, username_tagger, username_taggee, status)
+			VALUES ('${content}','${usernamex}','${usernamey}', ${approved});`
+		));
 		return true
-	})
+    } else {
+    	return false
+	}
 }
 
 let getComments = function(pid) {
@@ -113,12 +181,15 @@ module.exports = {
 	validateLogin,
 	register,
 	userGroups,
+	getVisibleContent,
 	getUsernames,
 	addFriendToGroup,
 	addContent,
 	getComments,
 	getProposedTags,
-	acceptTag,
 	rejectTag,
-	createFriendGroup
+	createFriendGroup,
+	acceptTag,
+	addTag,
+	isVisible
 }
